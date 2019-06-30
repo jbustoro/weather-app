fe56ea4d82a31cafc39c77409baff6295ad4dd40
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2019-06-30 21:25:32 +0200

    Added blurred background image

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Weather from "../Weather/Weather";
+import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <div className="bg" />
         <Weather />
       </div>
     );
